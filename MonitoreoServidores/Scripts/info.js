5361947b625fb455d69ad0df37c4a381fa2b6d97
incorporado <incorporado@gmail.com>
2016-06-18 20:53:26 -0300

    valor 4

--- MonitoreoServidores/Scripts/info.js
+++ MonitoreoServidores/Scripts/info.js
@@ -9,7 +9,10 @@ var VBatMB = 30;
 var ZBatMB = 40;
 var ZPATMB = 50;
 var TCAFMB = 60;
-
+var stop;
+var maxVBatMB;  
+var maxZBatMB;
+var maxZPATMB;
 
 function inicializarEncabezado(codigo) {
     if (codigo == 1) {
@@ -26,18 +29,39 @@ function inicializarEncabezado(codigo) {
     }
 };
 
+
 $(document).ready(function () {
     document.getElementById("VBat").innerHTML = 0;
     document.getElementById("ZBat").innerHTML = 0;
     document.getElementById("ZPAT").innerHTML = 0;
-    var maxVBatMB = VBatMB * 1.3;
-    var maxZBatMB = ZBatMB * 1.3;
-    var maxZPATMB = ZPATMB * 1.3;
-    while ((parseInt(document.getElementById("VBat").innerHTML) < maxVBatMB) && (parseInt(document.getElementById("ZBat").innerHTML) < maxZBatMB) && (parseInt(document.getElementById("ZPAT").innerHTML) < maxZPATMB)) {
-        document.getElementById("VBat").innerHTML = parseInt(parseInt(document.getElementById("VBat").innerHTML) + 1);
-        document.getElementById("ZBat").innerHTML = parseInt(parseInt(document.getElementById("ZBat").innerHTML) + 1);
-        document.getElementById("ZPAT").innerHTML = parseInt(parseInt(document.getElementById("ZPAT").innerHTML) + 1);
-    }
+    maxVBatMB = VBatMB * 1.3;
+    maxZBatMB = ZBatMB * 1.3;
+    maxZPATMB = ZPATMB * 1.3;
+    var i = 0;
+    stop = setInterval('loop()', 1000);
+    
 });
 
-function cheqInitState(){}
+function stopInterval() {
+    
+    if ((parseFloat(document.getElementById("VBat").innerHTML) > maxVBatMB) || (parseFloat(document.getElementById("ZBat").innerHTML) > maxZBatMB) || (parseFloat(document.getElementById("ZPAT").innerHTML) > maxZPATMB)) {
+        clearInterval(stop);
+        console.log("hola1");
+    }
+}
+
+function cheqInitState() { }
+
+function loop() {
+
+    document.getElementById("VBat").innerHTML = parseFloat(parseFloat(document.getElementById("VBat").innerHTML) + 1);
+
+    var x = parseFloat(parseFloat(document.getElementById("ZBat").innerHTML) + 0.1);
+    document.getElementById("ZBat").innerHTML = x.toFixed(2);
+
+    x = parseFloat(parseFloat(document.getElementById("ZPAT").innerHTML) + 0.01);
+    document.getElementById("ZPAT").innerHTML = x.toFixed(2);
+
+    console.log("hola2");
+    stopInterval();
+}
