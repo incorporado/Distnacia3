57f576fe8a5eb83fc67c766d930208bd7ff23083
incorporado <incorporado@gmail.com>
2016-06-18 16:47:54 -0300

    valor de equipo 1

--- MonitoreoServidores/Scripts/info.js
+++ MonitoreoServidores/Scripts/info.js
@@ -5,10 +5,10 @@ var MarcaMB = "Elvison";
 var OrdendeCompraMB = "12600002";
 var NumeroSerieMB = "120002";
 var TipoMB = "Vertical";
-var VBatMB = 3;
-var ZBatMB = 4;
-var ZPATMB = 5;
-var TCAFMB = 6;
+var VBatMB = 30;
+var ZBatMB = 40;
+var ZPATMB = 50;
+var TCAFMB = 60;
 
 
 function inicializarEncabezado(codigo) {
@@ -25,8 +25,25 @@ function inicializarEncabezado(codigo) {
             Subestacion = "Subestacion CABA La Plata";
     }
 };
+
 $(document).ready(function () {
-    
+    $("#VBat").html(0);
+    $("#VBat").html(0);
+    $("#VBat").html(0);
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    var maxVBatMB = VBatMB * 1.3;
+    var maxZBatMB = ZBatMB * 1.3;
+    var maxZPATMB = ZPATMB * 1.3;
+    while (($("#VBat").html() < maxVBatMB) || ($("#ZBat").html() < maxZBatMB) || ($("#ZPAT").html() < maxZPATMB)) {
+        $("#VBat").html(i);
+        $("#ZBat").html(j);
+        $("#ZPAT").html(k);
+        i = i + 1;
+        j = j + 0.1;
+        k = k + 0.01;
+    }
 });
 
 function cheqInitState(){}
