719999504c6454cae2cf8f5e2ef4bebb5f0f6d3e
incorporado <incorporado@gmail.com>
2016-06-21 19:38:43 -0300

    advertencia 1

--- MonitoreoServidores/Scripts/info.js
+++ MonitoreoServidores/Scripts/info.js
@@ -10,15 +10,19 @@ var ZBatMB = 20;
 var ZPATMB = 30;
 var TCAFMB = 40;
 var stop;
-var maxVBatMB;  
-var maxZBatMB;
-var maxZPATMB;
+var maxVBatMB = VBatMB * 1.3;
+var maxZBatMB = ZBatMB * 1.3;
+var maxZPATMB = ZPATMB * 1.3;
+var minVBatMB = VBatMB * 0.7;
+var minZBatMB = ZBatMB * 0.7;
+var minZPATMB = ZPATMB * 0.7;
 var aux;
 var aux1;
 var aux2;
 var parar1;
 var parar2;
 var parar3;
+
 
 function inicializarEncabezado(codigo) {
     if (codigo == 1) {
@@ -40,14 +44,16 @@ $(document).ready(function () {
     document.getElementById("VBat").innerHTML = 0;
     document.getElementById("ZBat").innerHTML = 0;
     document.getElementById("ZPAT").innerHTML = 0;
-    maxVBatMB = VBatMB * 1.3;
-    maxZBatMB = ZBatMB * 1.3;
-    maxZPATMB = ZPATMB * 1.3;
-    var i = 0;
     ocultarRuedaOpcion2();
     stop = setInterval('loop()', 1000);
     
 });
+
+//0-6 nada
+//7-10 advertencia
+//10 ref
+//11-14 maximo fisico
+
 
 function stopInterval() {    
     if ((parseFloat(document.getElementById("VBat").innerHTML) > maxVBatMB) || (parseFloat(document.getElementById("ZBat").innerHTML) > maxZBatMB) || (parseFloat(document.getElementById("ZPAT").innerHTML) > maxZPATMB)) {
@@ -55,16 +61,18 @@ function stopInterval() {
     }
 }
 
+
 function cheqInitState() { }
+
 
 function loop() {
 
     document.getElementById("VBat").innerHTML = parseFloat(parseFloat(document.getElementById("VBat").innerHTML) + 1);
 
-    var x = parseFloat(parseFloat(document.getElementById("ZBat").innerHTML) + 0.1);
+    var x = parseFloat(parseFloat(document.getElementById("ZBat").innerHTML) + 1.3);
     document.getElementById("ZBat").innerHTML = x.toFixed(2);
 
-    x = parseFloat(parseFloat(document.getElementById("ZPAT").innerHTML) + 0.01);
+    x = parseFloat(parseFloat(document.getElementById("ZPAT").innerHTML) + 0.1);
     document.getElementById("ZPAT").innerHTML = x.toFixed(2);
 
     stopInterval();
@@ -74,9 +82,14 @@ function loop() {
     aux = parseFloat(document.getElementById("VBat").innerHTML);
 }
 
+
 function cambiarImagen() {
     if (parseFloat(document.getElementById("VBat").innerHTML) > VBatMB) {
         document.getElementById('PPATAl').src = "images/NO.gif";
+    }
+    else if ((parseFloat(document.getElementById("VBat").innerHTML) > minVBatMB) &&
+        (parseFloat(document.getElementById("VBat").innerHTML) <= VBatMB)) {
+        document.getElementById('PPATAl').src = "images/undefined.jpg";
     }
     else {
         document.getElementById('opciones').disabled = false;
@@ -89,22 +102,35 @@ function cambiarImagen() {
         document.getElementById('PPATAl').src = "images/SI.gif";
     }
 
+
+
     if (parseFloat(document.getElementById("ZBat").innerHTML) > ZBatMB) {
         document.getElementById('NPATAl').src = "images/NO.gif";
+    }
+    else if ((parseFloat(document.getElementById("ZBat").innerHTML) > minZBatMB) &&
+    (parseFloat(document.getElementById("ZBat").innerHTML) <= ZBatMB)) {
+        document.getElementById('NPATAl').src = "images/undefined.jpg";
     }
     else {
         document.getElementById("opciones").disable = false;
         document.getElementById('NPATAl').src = "images/SI.gif";
     }
 
+
+
     if (parseFloat(document.getElementById("ZPAT").innerHTML) > ZPATMB) {
         document.getElementById('HZBatAl').src = "images/NO.gif";
+    }
+    else if ((parseFloat(document.getElementById("ZPAT").innerHTML) > minZPATMB) &&
+        (parseFloat(document.getElementById("ZPAT").innerHTML) <= ZZPATMB)) {
+        document.getElementById('HZBatAl').src = "images/undefined.jpg";
     }
     else {
         document.getElementById("opciones").disable = false;
         document.getElementById('HZBatAl').src = "images/SI.gif";
     }
 }
+
 
 function ValidaOpcion(combo) {
     var indice = combo.selectedIndex;
@@ -126,6 +152,7 @@ function ValidaOpcion(combo) {
     }
 }
 
+
 function restar() {
     aux = parseFloat(document.getElementById('VBat').innerHTML) - 0.5;    
 
@@ -139,6 +166,7 @@ function restar() {
     cambiarImagen();
 }
 
+
 function restar1() {
     aux1 = parseFloat(document.getElementById('VBat').innerHTML) - 1;
 
@@ -151,6 +179,7 @@ function restar1() {
 
     cambiarImagen();
 }
+
 
 function restar2() {
     aux2 = parseFloat(document.getElementById('VBat').innerHTML) - 0.1;
@@ -169,6 +198,7 @@ function restar2() {
 function ocultarRuedaOpcion2() {
     $("#ruedaopcion2").hide();
 }
+
 
 function reordenarListaVBat() {
     var textoTemporal = document.getElementById('opciones').options[1].innerHTML;
